176d84e98be4243fbdd1f299630116431a477055
Code-By-Adarsh <adarshmishra70931@gmail.com>
2026-08-27 18:35:28 +0530

    Configure playwright for env, except timeout and base url

--- Mprofy/playwright.config.js
+++ Mprofy/playwright.config.js
@@ -5,9 +5,9 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * @see https://playwright.dev/docs/test-configuration
@@ -39,7 +39,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'https://mprofy-web2-dev.vercel.app',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     screenshot:'on',
@@ -47,6 +47,10 @@ export default defineConfig({
     trace: 'on-first-retry',
     headless:true,
     viewport:{width:922,height:411},
+  },
+
+  expect:{
+    timeout:20000
   },
 
   /* Configure projects for major browsers */
